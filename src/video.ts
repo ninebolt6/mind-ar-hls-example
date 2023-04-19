93b667e657684b5850e8bb08847227dc63d2a019
--get <67471674+ninebolt6@users.noreply.github.com>
2023-04-19 10:37:44 +0900

    feat: use config

--- src/video.ts
+++ src/video.ts
@@ -1,11 +1,25 @@
 // @ts-expect-error
 import Hls from "hls.js/dist/hls.min.js";
 
-var video = document.getElementById("video") as HTMLMediaElement;
+const video = document.getElementById("video") as HTMLMediaElement;
+const config = {
+  manifestLoadingTimeOut: 2000, // マスターマニフェストのタイムアウト(ミリ秒)
+  manifestLoadingMaxRetry: 10, // マスターマニフェストを何回リトライするか
+  manifestLoadingMaxRetryTimeout: 3000, // タイムアウトしたあとリトライするまでの最大待機時間(ミリ秒)
+  levelLoadingTimeOut: 2000, // レベルマニフェストのタイムアウト(ミリ秒)
+  levelLoadingMaxRetry: 10, // レベルマニフェストを何回リトライするか
+  levelLoadingMaxRetryTimeout: 2000, // タイムアウトしたあとリトライするまでの最大待機時間(ミリ秒)
+  fragLoadingTimeOut: 2000, // 動画データのタイムアウト(ミリ秒)
+  fragLoadingMaxRetry: 10, // 動画データを何回リトライするか
+  fragLoadingMaxRetryTimeout: 2000, // 動画データがタイムアウトしたあとリトライするまでの最大待機時間(ミリ秒)
+  liveBackBufferLength: 0, // 再生し終わったデータを保持する長さ(秒)
+  debug: true,
+};
+
 const videoUrl =
   "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8";
 if (Hls.isSupported()) {
-  var hls = new Hls({ debug: true });
+  var hls = new Hls(config);
   hls.loadSource(videoUrl);
   hls.attachMedia(video);
   hls.on(Hls.Events.MANIFEST_PARSED, () => video.play());
