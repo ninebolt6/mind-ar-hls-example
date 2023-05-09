20b6e528ff8866a222ad37f64db108efd7e0823a
--get <67471674+ninebolt6@users.noreply.github.com>
2023-05-10 00:06:19 +0900

    fix: workaround for iOS

--- src/main.ts
+++ src/main.ts
@@ -14,15 +14,19 @@ const mindarThree = new MindARThree({
 });
 const { renderer, scene, camera } = mindarThree;
 const video = document.getElementById("video") as HTMLVideoElement;
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d")!;
+
 const anchor = mindarThree.addAnchor(0);
 // 映像を投影するテクスチャを作成
-const texture = new THREE.VideoTexture(video);
+const texture = new THREE.CanvasTexture(canvas);
 texture.minFilter = THREE.LinearFilter;
 texture.magFilter = THREE.LinearFilter;
 texture.format = THREE.RGBAFormat;
 const material = new THREE.MeshBasicMaterial({
   map: texture,
   transparent: true,
+  opacity: 0.8,
 });
 // 平面ジオメトリを作成して、テクスチャを貼り付ける
 const geometry = new THREE.PlaneGeometry(2, 2);
@@ -30,9 +34,20 @@ const mesh = new THREE.Mesh(geometry, material);
 anchor.group.add(mesh);
 
 const start = async () => {
-  await mindarThree.start();
   video.play();
+  await mindarThree.start();
   renderer.setAnimationLoop(() => {
+    // iOS Safari でテクスチャが透過表示されない問題のworkaround
+    // canvasに一度描画し直す
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.drawImage(
+      video as CanvasImageSourceWebCodecs,
+      0,
+      0,
+      ctx.canvas.width,
+      ctx.canvas.height
+    );
+    texture.needsUpdate = !video.paused;
     renderer.render(scene, camera);
   });
 };
